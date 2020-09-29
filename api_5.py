a706f20a2ea87d0eeb34e8ffa5eeaee073929331
ruk228 <ruk228@users.noreply.github.com>
2020-09-29 22:01:57 +0400

    numerous changes
    1 Отформатируйте код
    2 правильный регистр букв
    3 Высушите названия
    4 аргументы функции
    5 название функции с глагола
    6 точные названия
    7 JSON
    8 избыточные индексы
    9 Упростите код
    10 Очистите код от индексов
    11 копипасту в функцию

--- api_5.py
+++ api_5.py
@@ -1,21 +1,17 @@
 import requests
-import json
 import os
 from terminaltables import SingleTable
 from dotenv import load_dotenv
 
 
-
-def counting_data_for_statistics(the_total_salary,
-vacancies_processed, language, number_of_requests):
+def calculating_the_average_salary(
+    the_total_salary,
+        vacancies_processed):
     average_salary = (the_total_salary / vacancies_processed)
-    the_average_salary_for_language.append(average_salary)
-    languages.append(language)
-    number_of_vacancies.append(number_of_requests)
-    processed_vacancies_in_each_language.append(vacancies_processed)
+    return average_salary
 
 
-def link_to_the_HeadHunter_job_page(page, language, url_hh):
+def dictionary_of_site_HeadHunter(page, language, url_hh):
     payload = {'text': 'Программист {}'.format(
         language), 'area': '1', 'period': '30', 'per_page': '100',
         'page': '{}'.format(page), 'only_with_salary': 'True'}
@@ -24,18 +20,30 @@ def link_to_the_HeadHunter_job_page(page, language, url_hh):
     return response
 
 
-def predict_rub_salary(json_text, number_of_vacancies_per_page):
-    json_text = json_text['items'][number_of_vacancies_per_page]['salary']
-    return json_text
+def predict_rub_salary(information_on_vacancies, number_of_vacancies_per_page):
+    salary = information_on_vacancies['items'][
+        number_of_vacancies_per_page]['salary']
+    return salary
 
 
-def link_to_the_superjob_job_page(token, url_SJ, language):
+def dictionary_of_site_SJ(token, url_SJ, language):
     auth_token = {'X-Api-App-Id': '{}'.format(token)}
     params = {'keyword': '{}'.format(
         language), 'town': 4, 'catalogues': 48, 'count': 100}
     response = requests.get(url_SJ, headers=auth_token, params=params)
     response.raise_for_status()
     return response
+
+
+def processed_data_by_language(
+    information_about_a_specific_language,
+        language):
+    programming_languages_statistics = ([language, '{}'.format(
+          information_about_a_specific_language[0]['vacancies_found']), '{}'
+          .format(information_about_a_specific_language[0][
+            'vacancies_processed']), '{}'
+          .format(information_about_a_specific_language[0]['average_salary'])])
+    return programming_languages_statistics
 
 
 def predict_rub_salary_for_SuperJo(one_vacancy_in_SJ):
@@ -59,9 +67,9 @@ if __name__ == '__main__':
       'Scala',
       'Swift',
       'Typescript']
-    NUMBER_OF_LANGUAGES = 13
-    table_data_HH = []
-    table_data_SJ = []
+    number_of_languages = 13
+    table_of_information_about_languages_HH = []
+    table_of_information_about_languages_SJ = []
     token = os.getenv('TOKEN_SUPERJOB')
     the_average_salary_for_language = []
     languages = []
@@ -75,11 +83,11 @@ if __name__ == '__main__':
         full_salary = 0
         vacancies_processed = 0
         average_salary = 0
-        number_of_vacancies_SJ = len(link_to_the_superjob_job_page(
+        number_of_vacancies_SJ = len(dictionary_of_site_SJ(
             token, url_SJ, language).json()["objects"])
-        oll_vacancy_in_SJ = link_to_the_superjob_job_page(
+        oll_vacancy_in_SJ = dictionary_of_site_SJ(
             token, url_SJ, language).json()["objects"]
-        for separate_vacancy in range(0, number_of_vacancies_SJ):
+        for separate_vacancy in range(number_of_vacancies_SJ):
             one_vacancy_in_SJ = oll_vacancy_in_SJ[separate_vacancy]
             salary_jobs_SJ = predict_rub_salary_for_SuperJo(one_vacancy_in_SJ)
             if salary_jobs_SJ != 0:
@@ -88,41 +96,45 @@ if __name__ == '__main__':
             if vacancies_processed != 0:
                 average_salary = full_salary / vacancies_processed
             information_on_vacancies_in_SJ.update({
-                language: {'vacancies_found': number_of_vacancies_SJ,
-            'vacancies_processed': vacancies_processed, 'average_salary': int(
-                average_salary)}})
+                language: {
+                    'vacancies_found': number_of_vacancies_SJ,
+                    'vacancies_processed':
+                        vacancies_processed, 'average_salary': int(
+                            average_salary)}})
     for language in programming_languages:
         number_of_requests = 0
         page = 0
         vacancies_processed = 0
         average_salary = 0
         the_total_salary = 0
-        pages_number = link_to_the_HeadHunter_job_page(
+        pages_number = dictionary_of_site_HeadHunter(
             page, language, url_hh).json()['pages']
-        json_text = (json.loads(link_to_the_HeadHunter_job_page(
-            page, language, url_hh).text))
-        for iteration_by_number_of_pages in range(0, pages_number):
-            number_of_requests = number_of_requests + len(json_text['items'])
-            number_of_requests_per_page = len(json_text['items'])
-            while page + 1 < pages_number:
-                page += 1
+        information_on_vacancies = (dictionary_of_site_HeadHunter(
+            page, language, url_hh).json())
+        for iteration_by_number_of_pages in range(pages_number):
+            number_of_requests = number_of_requests + len(
+              information_on_vacancies['items'])
+            number_of_requests_per_page = len(
+              information_on_vacancies['items'])
             for number_of_vacancies_per_page in range(
-              0, number_of_requests_per_page):
+              number_of_requests_per_page):
                 salary_for_the_work = predict_rub_salary(
-                  json_text, number_of_vacancies_per_page)
+                  information_on_vacancies, number_of_vacancies_per_page)
                 if salary_for_the_work['from']:
                     vacancies_processed += 1
                     the_total_salary += salary_for_the_work['from']
-        counting_data_for_statistics(
-            the_total_salary, vacancies_processed, language, number_of_requests
-        )
-    for id_language in range(0, NUMBER_OF_LANGUAGES):
+        the_average_salary_for_language.append(calculating_the_average_salary(
+          the_total_salary, vacancies_processed))
+        languages.append(language)
+        number_of_vacancies.append(number_of_requests)
+        processed_vacancies_in_each_language.append(vacancies_processed)
+    for id_language in range(number_of_languages):
         information_on_vacancies_in_hh.update(({languages[id_language]: {
             'vacancies_found': number_of_vacancies[id_language],
             'vacancies_processed': processed_vacancies_in_each_language[
                 id_language], 'average_salary': int(
               the_average_salary_for_language[id_language])}}))
-    table_data_HH.append([
+    table_of_information_about_languages_HH.append([
           'Язык программирования',
           'Вакансий найдено',
           'Вакансий обработано',
@@ -130,16 +142,12 @@ if __name__ == '__main__':
     for language in information_on_vacancies_in_hh:
         information_about_a_specific_language = [
             information_on_vacancies_in_hh[language]]
-        table_data_HH.append([language, '{}'.format(
-                information_about_a_specific_language[0]
-                ['vacancies_found']), '{}'.format(
-                information_about_a_specific_language[0]
-                ['vacancies_processed']), '{}'.format(
-                information_about_a_specific_language[0]
-                ['average_salary'])])
+        table_of_information_about_languages_HH.append(
+            processed_data_by_language(
+                information_about_a_specific_language, language))
         table_instance_HH = SingleTable(
-            table_data_HH, 'HeadHunter Moscow')
-    table_data_SJ.append([
+            table_of_information_about_languages_HH, 'HeadHunter Moscow')
+    table_of_information_about_languages_SJ.append([
           'Язык программирования',
           'Вакансий найдено',
           'Вакансий обработано',
@@ -147,12 +155,11 @@ if __name__ == '__main__':
     for language in information_on_vacancies_in_SJ:
         information_about_a_specific_language = [
           information_on_vacancies_in_SJ[language]]
-        table_data_SJ.append([language, '{}'.format(
-          information_about_a_specific_language[0]['vacancies_found']), '{}'
-          .format(information_about_a_specific_language[0][
-            'vacancies_processed']), '{}'
-          .format(information_about_a_specific_language[0]['average_salary'])])
-        table_instance_SJ = SingleTable(table_data_SJ, 'SuperJob {}'.format(
-          one_vacancy_in_SJ["town"]['title']))
+        table_of_information_about_languages_SJ.append(
+          processed_data_by_language(
+                information_about_a_specific_language, language))
+        table_instance_SJ = SingleTable(
+                table_of_information_about_languages_SJ, 'SuperJob {}'.format(
+                    one_vacancy_in_SJ["town"]['title']))
     print(table_instance_SJ.table)
     print(table_instance_HH.table)
