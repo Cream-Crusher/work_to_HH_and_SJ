6a5edc8f9098c34b2ec02e2d77073ec4bfab87c8
Никита <64539102+ruk228@users.noreply.github.com>
2021-01-24 16:29:15 +0400

    split a large block of code into functions

--- working_with_websites.py
+++ working_with_websites.py
@@ -5,19 +5,6 @@ from hh import get_information_from_hh, get_vacancies_hh
 from sj import get_information_from_sj, get_vacancies_sj
 
 
-def get_collecting_information(processed_vacancies, average_salary,
-                              information_on_vacancies
-    ):
-    information_on_vacancies_for_the_table = {}
-    information_on_vacancies_for_the_table.update({
-        information_on_vacancies['language']: {
-        'vacancies_found': information_on_vacancies['found'],
-        'processed_vacancies': processed_vacancies,
-        'average_salary': average_salary}}
-    )
-    return information_on_vacancies_for_the_table
-
-
 def predict_rub_salary(pay_from, pay_to):
 
     if pay_from and pay_to:
@@ -30,50 +17,39 @@ def predict_rub_salary(pay_from, pay_to):
         return
 
 
-def get_building_table(information_on_vacancies, language,
-                        site_name, table_data
-                        ):
+def get_building_table(site_name, table_data, information_on_vacancies):
+
+    salary = 0
+    entrance = 0
+    processed_vacancies = information_on_vacancies[entrance]["found"]
+
+    for information_on_vacancies in information_on_vacancies:
+        pay_from = information_on_vacancies['pay_from']
+        pay_to = information_on_vacancies['pay_to']
+
+        if information_on_vacancies['currency'] == 'RUR' or pay_from or pay_to and \
+            information_on_vacancies['currency'] == 'rub':
+
+            salary += predict_rub_salary(pay_from, pay_to)
+
+        else:
+            processed_vacancies -= 1
+
+    average_salary = int(salary / processed_vacancies)
     table_parameters = (
         language,
-        information_on_vacancies[language]['vacancies_found'],
-        information_on_vacancies[language]['processed_vacancies'],
-        information_on_vacancies[language]['average_salary']
+        information_on_vacancies['found'],
+        processed_vacancies,
+        average_salary
         )
     table_data.append(table_parameters)
     table_instance = SingleTable(table_data, '{} Moscow'.format(site_name))
+
     return table_instance
-
-
-def get_salary_and_vacancies_processed(information_on_vacancies,
-                                        processed_vacancies
-                                        ):
-    salary_and_vacancies_processed = {}
-    pay_from = information_on_vacancies['pay_from']
-    pay_to = information_on_vacancies['pay_to']
-    salary = 0
-
-    if information_on_vacancies['currency'] == 'RUR' or pay_from or \
-        pay_to and information_on_vacancies['currency'] == 'rub':
-
-        salary = predict_rub_salary(pay_from, pay_to)
-
-    else:
-        processed_vacancies -= 1
-
-    salary_and_vacancies_processed = {'salary': salary,
-                                      'processed_vacancies': processed_vacancies
-                                      }
-    return salary_and_vacancies_processed
 
 
 if __name__ == '__main__':
     load_dotenv()
-    token = os.getenv('TOKEN_SUPERJOB')
-    table_data = [[
-        'Язык программирования',
-        'Вакансий найдено',
-        'Вакансий обработано',
-        'Средняя зарплата']]
     programming_languages = [
         'JavaScript',
         'Java',
@@ -87,32 +63,23 @@ if __name__ == '__main__':
         'Go',
         'Swift',
         'Typescript']
-    entrance = 0
+    site_name = 'HeadHunter'
+    table_data = [[
+        'Язык программирования',
+        'Вакансий найдено',
+        'Вакансий обработано',
+        'Средняя зарплата']]
 
     for language in programming_languages:
         all_vacancies = get_information_from_hh(language)
-        site_name = 'HeadHunter'
         information_on_vacancies = get_vacancies_hh(all_vacancies, language)
-        processed_vacancies = information_on_vacancies[entrance]["found"]
-        salary = 0
-
-        for information_on_vacancies in information_on_vacancies:
-            salary_and_vacancies_processed = get_salary_and_vacancies_processed(
-                                            information_on_vacancies, processed_vacancies
-                                            )
-            salary += salary_and_vacancies_processed['salary']
-            processed_vacancies = salary_and_vacancies_processed['processed_vacancies']
-
-
-        average_salary = int(salary / processed_vacancies)
-
-        information_on_vacancies = get_collecting_information(processed_vacancies, average_salary,                                                              information_on_vacancies
-                                                            )
-        table_with_data_on_vacancies_hh = get_building_table(information_on_vacancies,
-                                                            language, site_name, table_data
+        table_with_data_on_vacancies_hh = get_building_table(site_name, table_data,
+                                                            information_on_vacancies
                                                             )
     print(table_with_data_on_vacancies_hh.table)
 
+    token = os.getenv('TOKEN_SUPERJOB')
+    site_name = 'SuperJob'
     table_data = [[
         'Язык программирования',
         'Вакансий найдено',
@@ -122,25 +89,7 @@ if __name__ == '__main__':
     for language in programming_languages:
         all_vacancies = get_information_from_sj(language, token)
         information_on_vacancies = get_vacancies_sj(all_vacancies, language)
-        site_name = 'SuperJob'
-        salary = 0
-        processed_vacancies = information_on_vacancies[entrance]['found']
-
-        for information_on_vacancies in information_on_vacancies:
-
-            salary_and_vacancies_processed = get_salary_and_vacancies_processed(information_on_vacancies,
-                                                                                processed_vacancies
-                                                                                )
-
-            salary += salary_and_vacancies_processed['salary']
-            processed_vacancies = salary_and_vacancies_processed['processed_vacancies']
-
-        average_salary = int(salary / processed_vacancies)
-        information_on_vacancies = get_collecting_information(processed_vacancies,
-                                                            average_salary, information_on_vacancies
+        table_with_data_on_vacancies_sj = get_building_table(site_name, table_data,
+                                                            information_on_vacancies
                                                             )
-        table_with_data_on_vacancies_sj = get_building_table(information_on_vacancies,
-                                                            language, site_name, table_data
-                                                            )
-
     print(table_with_data_on_vacancies_sj.table)
