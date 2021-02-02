a247a4175bf9d0a9a7a58ec8a430ad8cbefd2ff8
Никита <64539102+ruk228@users.noreply.github.com>
2021-02-02 20:31:28 +0400

    fallback option

--- hh.py
+++ hh.py
@@ -2,35 +2,35 @@ import requests
 
 
 def get_information_from_hh(language):
+
     hh_url = 'https://api.hh.ru/vacancies'
     page = 0
     vacancies = []
-    pages = 100
-    city = 1
+    vacancies_the_page = 100
+    city_number = 1
     entrance = 0
     params = {
         'text': 'Программист {}'.format(language),
-        'area': city,
+        'area': city_number,
         'page': page,
         'only_with_salary': 'True'}
 
-    while page < pages:
+    while page < vacancies_the_page:
         params['page'] = page
         response = requests.get(hh_url, params=params)
         response.raise_for_status()
         vacancies.append(response.json())
-        pages = vacancies[entrance]['pages']
+        vacancies_the_page = vacancies[entrance]['pages']
         page += 1
 
     return vacancies
 
 
 def get_vacancies_hh(all_vacancies, language):
-    information_on_vacancies = []
-    vacancies = []
-    vacancies.extend(all_vacancies)
 
-    for part_vacancies in vacancies:
+    information_on_vacancies = []
+    
+    for part_vacancies in all_vacancies:
         for vacancies in part_vacancies['items']:
             pay_from = vacancies['salary']['from']
             pay_to = vacancies['salary']['to']
--- working_with_websites.py
+++ working_with_websites.py
@@ -1,4 +1,5 @@
 import os
+import copy
 from terminaltables import SingleTable
 from dotenv import load_dotenv
 from hh import get_information_from_hh, get_vacancies_hh
@@ -17,7 +18,7 @@ def predict_rub_salary(pay_from, pay_to):
         return
 
 
-def get_building_table(site_name, table_data, information_on_vacancies):
+def get_information_for_the_table(information_on_vacancies):
 
     salary = 0
     entrance = 0
@@ -36,9 +37,25 @@ def get_building_table(site_name, table_data, information_on_vacancies):
             processed_vacancies -= 1
 
     average_salary = int(salary / processed_vacancies)
+    information_for_the_table = {'processed_vacancies': processed_vacancies,
+                                    'found': information_on_vacancies['found'],
+                                    'average_salary': average_salary
+                                    }
+    return information_for_the_table
+
+
+def building_table(site_name, header_table, information_on_vacancies, language):
+
+    table_data = copy.copy(header_table)
+    print(language)
+    information_for_the_table = get_information_for_the_table(information_on_vacancies)
+    #for information_on_vacancies in information_on_vacancies:
+    found = information_for_the_table['found']
+    processed_vacancies = information_for_the_table['processed_vacancies']
+    average_salary = information_for_the_table['average_salary']
     table_parameters = (
         language,
-        information_on_vacancies['found'],
+        found,
         processed_vacancies,
         average_salary
         )
@@ -48,8 +65,53 @@ def get_building_table(site_name, table_data, information_on_vacancies):
     return table_instance
 
 
+def get_table_hh(programming_languages):
+
+    site_name = 'HeadHunter'
+    header_table = [[
+        'Язык программирования',
+        'Вакансий найдено',
+        'Вакансий обработано',
+        'Средняя зарплата']]
+    information_on_vacancies = []
+    
+    for language in programming_languages:
+        all_vacancies = get_information_from_hh(language)
+        information_on_vacancies = get_vacancies_hh(all_vacancies, language)
+
+    table_with_data_on_vacancies_hh = building_table(site_name, header_table,
+                                                    information_on_vacancies,
+                                                    language
+                                                    )
+    print(table_with_data_on_vacancies_hh.table)
+
+
+def get_table_sj(programming_languages):
+
+    token = os.getenv('TOKEN_SUPERJOB')
+    site_name = 'SuperJob'
+    header_table = [[
+        'Язык программирования',
+        'Вакансий найдено',
+        'Вакансий обработано',
+        'Средняя зарплата']]
+    information_on_vacancies = []
+
+    for language in programming_languages:
+        all_vacancies = get_information_from_sj(language, token)
+        information_on_vacancies = get_vacancies_sj(all_vacancies, language)
+
+    table_with_data_on_vacancies_sj = building_table(site_name, header_table,
+                                                    information_on_vacancies,
+                                                    language
+                                                    )
+    print(table_with_data_on_vacancies_sj.table)
+
+
 if __name__ == '__main__':
     load_dotenv()
+    programming_languages = ['Go', 'Go']
+    '''
     programming_languages = [
         'JavaScript',
         'Java',
@@ -63,33 +125,6 @@ if __name__ == '__main__':
         'Go',
         'Swift',
         'Typescript']
-    site_name = 'HeadHunter'
-    table_data = [[
-        'Язык программирования',
-        'Вакансий найдено',
-        'Вакансий обработано',
-        'Средняя зарплата']]
-
-    for language in programming_languages:
-        all_vacancies = get_information_from_hh(language)
-        information_on_vacancies = get_vacancies_hh(all_vacancies, language)
-        table_with_data_on_vacancies_hh = get_building_table(site_name, table_data,
-                                                            information_on_vacancies
-                                                            )
-    print(table_with_data_on_vacancies_hh.table)
-
-    token = os.getenv('TOKEN_SUPERJOB')
-    site_name = 'SuperJob'
-    table_data = [[
-        'Язык программирования',
-        'Вакансий найдено',
-        'Вакансий обработано',
-        'Средняя зарплата']]
-
-    for language in programming_languages:
-        all_vacancies = get_information_from_sj(language, token)
-        information_on_vacancies = get_vacancies_sj(all_vacancies, language)
-        table_with_data_on_vacancies_sj = get_building_table(site_name, table_data,
-                                                            information_on_vacancies
-                                                            )
-    print(table_with_data_on_vacancies_sj.table)
+    '''
+    get_table_hh(programming_languages)
+    #get_table_sj(programming_languages)
